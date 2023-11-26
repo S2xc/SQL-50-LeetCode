9bc105eef8f91508a750504c6075ff4b079b26ec
S2xc <77986396+S2xc@users.noreply.github.com>
2023-11-27 00:24:00 +0300

    Create 197. Rising Temperature.sql

---
+++ Basic Joins/197. Rising Temperature.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+SELECT C.id
+FROM Weather AS P
+INNER JOIN Weather AS C ON DATE_ADD(P.recordDate, INTERVAL 1 DAY) = C.recordDate
+    AND C.Temperature > P.Temperature
