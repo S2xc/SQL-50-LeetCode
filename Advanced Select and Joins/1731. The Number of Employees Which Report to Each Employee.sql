b30ded6723460d9d6f62425f6db5a0441527ca09
S2xc <77986396+S2xc@users.noreply.github.com>
2024-02-01 21:21:48 +0300

    Update 1731. The Number of Employees Which Report to Each Employee.sql
    1.02.2024

--- Advanced Select and Joins/1731. The Number of Employees Which Report to Each Employee.sql
+++ Advanced Select and Joins/1731. The Number of Employees Which Report to Each Employee.sql
@@ -1,1 +1,20 @@
+-- Write your PostgreSQL query statement below
 
+with tbl3 as (
+    select t1.reports_to,
+    count(t1.reports_to) as reports_count,
+    round(avg(t1.age)) as average_age
+    from Employees as t1
+    full join Employees as t2
+        on t2.employee_id = t1.reports_to
+    group by t1.reports_to
+)
+
+select Employees.employee_id,
+Employees.name,
+max(tbl3.reports_count) as reports_count,
+tbl3.average_age
+from Employees, tbl3
+where Employees.employee_id = tbl3.reports_to
+group by Employees.employee_id, Employees.name, tbl3.average_age
+order by Employees.employee_id
