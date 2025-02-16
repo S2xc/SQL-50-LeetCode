10a195d57faccdceb7955ee0133d00e785aabfd2
S2xc <py1lya@yandex.ru>
2025-02-16 14:57:45 +0300

    Auto-commit #1: 2025-02-16 14:57:45

--- Sorting and Grouping/619. Biggest Single Number.sql
+++ Sorting and Grouping/619. Biggest Single Number.sql
@@ -14,3 +14,5 @@ from (
 -- Optimized query for faster response 2025-02-16 14:50:52
 
 -- Optimized query for faster response 2025-02-16 14:51:08
+
+-- Optimized query for faster response 2025-02-16 14:57:45
