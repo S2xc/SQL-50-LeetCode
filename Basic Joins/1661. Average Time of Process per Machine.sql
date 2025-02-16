c678dbc3b230be64e6c24ecaa76a73ad020dd4ef
S2xc <py1lya@yandex.ru>
2025-02-16 14:50:18 +0300

    Auto-commit #6: 2025-02-16 14:50:18

--- Basic Joins/1661. Average Time of Process per Machine.sql
+++ Basic Joins/1661. Average Time of Process per Machine.sql
@@ -14,3 +14,5 @@ group by 1;
 -- Optimized query for faster response 2025-02-16 14:46:07
 
 -- Optimized query for faster response 2025-02-16 14:46:07
+
+-- Optimized query for faster response 2025-02-16 14:50:18
--- Select/595. Big Countries.sql
+++ Select/595. Big Countries.sql
@@ -11,3 +11,5 @@ population >= 25000000
 -- Optimized query for faster response 2025-02-16 14:46:07
 
 -- Optimized query for faster response 2025-02-16 14:50:13
+
+-- Optimized query for faster response 2025-02-16 14:50:18
