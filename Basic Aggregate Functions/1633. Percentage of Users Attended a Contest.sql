d05b010eafa227b5a120f722184ec286e61ba717
S2xc <py1lya@yandex.ru>
2025-02-16 14:58:49 +0300

    Auto-commit #16: 2025-02-16 14:58:49

--- Basic Aggregate Functions/1633. Percentage of Users Attended a Contest.sql
+++ Basic Aggregate Functions/1633. Percentage of Users Attended a Contest.sql
@@ -17,3 +17,5 @@ order by percentage desc, Register.contest_id asc
 -- Optimized query for faster response 2025-02-16 14:46:07
 
 -- Optimized query for faster response 2025-02-16 14:50:07
+
+-- Optimized query for faster response 2025-02-16 14:58:49
--- Select/1148. Article Views I.sql
+++ Select/1148. Article Views I.sql
@@ -8,3 +8,5 @@ order by author_id
 -- Optimized query for faster response 2025-02-16 14:50:20
 
 -- Optimized query for faster response 2025-02-16 14:50:22
+
+-- Optimized query for faster response 2025-02-16 14:58:49
