78599099c2aaea414025e5b7a109c458e4ac56d3
S2xc <py1lya@yandex.ru>
2025-02-16 14:50:16 +0300

    Auto-commit #5: 2025-02-16 14:50:15

--- Advanced String Functions/1527. Patients With a Condition.sql
+++ Advanced String Functions/1527. Patients With a Condition.sql
@@ -4,3 +4,5 @@ where conditions like 'DIAB1%' or
 conditions like '% DIAB1%'
 
 -- Optimized query for faster response 2025-02-16 14:15:05
+
+-- Optimized query for faster response 2025-02-16 14:50:16
--- Sorting and Grouping/596. Classes More Than 5 Students.sql
+++ Sorting and Grouping/596. Classes More Than 5 Students.sql
@@ -7,3 +7,5 @@ having count(class) > 4
 -- Optimized query for faster response 2025-02-16 14:15:00
 
 -- Optimized query for faster response 2025-02-16 14:42:18
+
+-- Optimized query for faster response 2025-02-16 14:50:16
