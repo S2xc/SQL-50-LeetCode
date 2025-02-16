4a25b00b97a731fea941217e190fe656e352ed10
S2xc <py1lya@yandex.ru>
2025-02-16 15:09:56 +0300

    Auto-commit #2: 2025-02-16 15:09:56

--- Advanced String Functions/176. Second Highest Salary.sql
+++ Advanced String Functions/176. Second Highest Salary.sql
@@ -9,3 +9,5 @@ WHERE Salary <> (SELECT max(salary) FROM Employee)
 -- Optimized query for faster response 2025-02-16 14:58:42
 
 -- Optimized query for faster response 2025-02-16 15:01:41
+
+-- Optimized query for faster response 2025-02-16 15:09:56
--- Select/584. Find Customer Referee.sql
+++ Select/584. Find Customer Referee.sql
@@ -9,3 +9,5 @@ referee_id is null
 -- Optimized query for faster response 2025-02-16 14:50:13
 
 -- Optimized query for faster response 2025-02-16 14:58:37
+
+-- Optimized query for faster response 2025-02-16 15:09:56
