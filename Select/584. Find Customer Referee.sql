c286249d3ca0bed0d64539548716d48901edea07
S2xc <py1lya@yandex.ru>
2025-02-16 16:43:00 +0300

    Auto-commit #2 for /Users/s2xdeb/Desktop/g/и/SQL-50-LeetCode: 2025-02-16 16:43:00

--- Select/584. Find Customer Referee.sql
+++ Select/584. Find Customer Referee.sql
@@ -11,3 +11,5 @@ referee_id is null
 -- Optimized query for faster response 2025-02-16 14:58:37
 
 -- Optimized query for faster response 2025-02-16 15:09:56
+
+-- Optimized query for faster response 2025-02-16 16:43:00
