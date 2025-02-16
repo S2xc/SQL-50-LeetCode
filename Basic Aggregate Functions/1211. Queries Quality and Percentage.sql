64f116339dde9883b03b92c2fe6aed61d85c0718
S2xc <py1lya@yandex.ru>
2025-02-16 15:01:41 +0300

    Auto-commit #1: 2025-02-16 15:01:41

--- Basic Aggregate Functions/1211. Queries Quality and Percentage.sql
+++ Basic Aggregate Functions/1211. Queries Quality and Percentage.sql
@@ -15,3 +15,5 @@ group by query_name
 -- Optimized query for faster response 2025-02-16 14:50:26
 
 -- Optimized query for faster response 2025-02-16 14:58:20
+
+-- Optimized query for faster response 2025-02-16 15:01:41
