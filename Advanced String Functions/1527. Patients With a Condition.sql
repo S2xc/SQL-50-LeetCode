73b1c4a779ad0e7197e388628cd78449c7911c1f
S2xc <py1lya@yandex.ru>
2025-02-16 16:43:24 +0300

    Auto-commit #13 for /Users/s2xdeb/Desktop/g/и/SQL-50-LeetCode: 2025-02-16 16:43:24

--- Advanced String Functions/1527. Patients With a Condition.sql
+++ Advanced String Functions/1527. Patients With a Condition.sql
@@ -8,3 +8,5 @@ conditions like '% DIAB1%'
 -- Optimized query for faster response 2025-02-16 14:50:16
 
 -- Optimized query for faster response 2025-02-16 15:11:06
+
+-- Optimized query for faster response 2025-02-16 16:43:24
