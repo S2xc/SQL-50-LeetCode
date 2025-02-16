08cb4128d8b860c2ee32e65524b3935d45dfff3f
S2xc <py1lya@yandex.ru>
2025-02-16 15:15:28 +0300

    Auto-commit #2 for /Users/s2xdeb/Desktop/g/и/SQL-50-LeetCode: 2025-02-16 15:15:28

--- Subqueries/1978. Employees Whose Manager Left the Company.sql
+++ Subqueries/1978. Employees Whose Manager Left the Company.sql
@@ -18,3 +18,5 @@ order by Employees.employee_id
 -- Optimized query for faster response 2025-02-16 14:42:25
 
 -- Optimized query for faster response 2025-02-16 14:58:28
+
+-- Optimized query for faster response 2025-02-16 15:15:28
