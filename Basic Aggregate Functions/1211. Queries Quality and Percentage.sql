9a2121d64b483b235812bee08cc446c582851c55
S2xc <py1lya@yandex.ru>
2025-02-17 20:03:20 +0300

    refactor: simplify error handling

--- Basic Aggregate Functions/1211. Queries Quality and Percentage.sql
+++ Basic Aggregate Functions/1211. Queries Quality and Percentage.sql
@@ -3,6 +3,8 @@ select query_name,
 round(avg(cast(rating as decimal) / position), 2) as quality,
 round(sum(
   case
+
+-- Optimized query for faster response - 2025-02-17 20:03:20
     when rating < 3 then 1 else 0 
   end) * round(100, 2) / 
 round(COUNT(*), 2), 2) AS poor_query_percentage
