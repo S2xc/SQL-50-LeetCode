56522081efa94e798bd2f1342c11c67fde45a4c0
S2xc <py1lya@yandex.ru>
2025-02-17 20:14:30 +0300

    feat(): implement new SQL-50-LeetCode functionality

--- Sorting and Grouping/2356. Number of Unique Subjects Taught by Each Teacher.sql
+++ Sorting and Grouping/2356. Number of Unique Subjects Taught by Each Teacher.sql
@@ -11,3 +11,5 @@ group by teacher_id
 -- Optimized query for faster response 2025-02-16 14:53:11
 
 -- Optimized query for faster response 2025-02-17 19:20:29
+
+-- Optimized query for faster response - 2025-02-17 20:14:30
