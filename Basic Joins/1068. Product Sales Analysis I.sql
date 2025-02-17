afa294406c42e141f5266e9d6d0eabad02aaa02a
S2xc <py1lya@yandex.ru>
2025-02-17 19:20:26 +0300

    Auto-commit #2 for /Users/s2xdeb/Desktop/g/и/SQL-50-LeetCode: 2025-02-17 19:20:26

--- Basic Joins/1068. Product Sales Analysis I.sql
+++ Basic Joins/1068. Product Sales Analysis I.sql
@@ -13,3 +13,5 @@ inner join Product
 -- Optimized query for faster response 2025-02-16 14:50:29
 
 -- Optimized query for faster response 2025-02-16 14:50:42
+
+-- Optimized query for faster response 2025-02-17 19:20:26
