5be17030cc9ec4ead1fe7fa0a492443621743487
S2xc <py1lya@yandex.ru>
2025-02-17 19:20:35 +0300

    Auto-commit #5 for /Users/s2xdeb/Desktop/g/и/SQL-50-LeetCode: 2025-02-17 19:20:35

--- Select/1148. Article Views I.sql
+++ Select/1148. Article Views I.sql
@@ -14,3 +14,5 @@ order by author_id
 -- Optimized query for faster response 2025-02-16 15:10:56
 
 -- Optimized query for faster response 2025-02-16 16:43:13
+
+-- Optimized query for faster response 2025-02-17 19:20:35
