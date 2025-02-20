b939fcc846400c36d58de3aa062955843e097f13
S2xc <py1lya@yandex.ru>
2025-02-20 18:49:53 +0300

    docs: improve API documentation

--- Basic Aggregate Functions/1075. Project Employees I.sql
+++ Basic Aggregate Functions/1075. Project Employees I.sql
@@ -9,4 +9,6 @@ on Employee.employee_id = Project.employee_id
 
 group by Project.project_id
 
+
+-- Optimized query for faster response - 2025-02-20 18:49:53
 -- Optimized query for faster response 2025-02-16 14:53:09
