cceef213d148173ad66e51e5b85ff6f1195eb3a7
S2xc <py1lya@yandex.ru>
2025-02-20 18:49:48 +0300

    docs(): update SQL-50-LeetCode documentation

--- Advanced Select and Joins/1789. Primary Department for Each Employee.sql
+++ Advanced Select and Joins/1789. Primary Department for Each Employee.sql
@@ -47,3 +47,5 @@ order by employee_id
 -- Optimized query for faster response 2025-02-16 15:11:10
 
 -- Optimized query for faster response 2025-02-16 15:11:14
+
+-- Optimized query for faster response - 2025-02-20 18:49:48
