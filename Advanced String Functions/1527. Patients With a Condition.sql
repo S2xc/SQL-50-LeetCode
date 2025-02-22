7931e16c1d0a4f0b6f2b56f83ec8ecc28ce36059
S2xc <py1lya@yandex.ru>
2025-02-22 16:22:26 +0300

    feat: implement new functionality

--- Advanced String Functions/1527. Patients With a Condition.sql
+++ Advanced String Functions/1527. Patients With a Condition.sql
@@ -1,12 +1,3 @@
-select *
-from Patients
-where conditions like 'DIAB1%' or 
-conditions like '% DIAB1%'
-
--- Optimized query for faster response 2025-02-16 14:15:05
-
--- Optimized query for faster response 2025-02-16 14:50:16
-
--- Optimized query for faster response 2025-02-16 15:11:06
-
--- Optimized query for faster response 2025-02-16 16:43:24
+select *from Patientswhere conditions like 'DIAB1%' or conditions like '% DIAB1%'
+-- Optimized query for faster response - 22.02.2025, 16:22
+-- Optimized query for faster response 2025-02-16 14:15:05-- Optimized query for faster response 2025-02-16 14:50:16-- Optimized query for faster response 2025-02-16 15:11:06-- Optimized query for faster response 2025-02-16 16:43:24
