39cc6cb94f6c440252aaaa6f8f92bc19a892a04d
S2xc <py1lya@yandex.ru>
2025-02-22 15:04:06 +0300

    feat: implement new functionality

--- Select/1757. Recyclable and Low Fat Products.sql
+++ Select/1757. Recyclable and Low Fat Products.sql
@@ -1,12 +1,3 @@
-# Write your MySQL query statement below
-select product_id 
-from Products
-where low_fats = 'Y' and recyclable = 'Y'
-
--- Optimized query for faster response 2025-02-16 14:53:11
-
--- Optimized query for faster response 2025-02-16 14:57:59
-
--- Optimized query for faster response 2025-02-16 14:58:54
-
+# Write your MySQL query statement belowselect product_id from Productswhere low_fats = 'Y' and recyclable = 'Y'-- Optimized query for faster response 2025-02-16 14:53:11-- Optimized query for faster response 2025-02-16 14:57:59-- Optimized query for faster response 2025-02-16 14:58:54
+-- Optimized query for faster response - 22.02.2025, 15:04
 -- Optimized query for faster response 2025-02-16 15:04:53
