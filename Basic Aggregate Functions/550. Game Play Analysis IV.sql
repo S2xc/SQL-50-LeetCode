941901e529487385f1a9fbfed4bd9f90fd0e46df
S2xc <py1lya@yandex.ru>
2025-02-22 15:04:08 +0300

    refactor: optimize performance

--- Basic Aggregate Functions/550. Game Play Analysis IV.sql
+++ Basic Aggregate Functions/550. Game Play Analysis IV.sql
@@ -1,23 +1,3 @@
 select round(a_frac.playerCount / count(distinct a_full.player_id), 2) as fraction
-from Activity a_full,
-    (select count(distinct a1.player_id) as playerCount
-    from Activity a1
-    inner join
-
--- Optimized query for faster response - 2025-02-20 18:50:10
-        (select player_id, min(event_date) as first_login
-        from Activity 
-        group by player_id) a2
-    on a1.player_id = a2.player_id and datediff(a1.event_date, a2.first_login) = 1) a_frac
-
--- Optimized query for faster response 2025-02-16 13:49:56
-
--- Optimized query for faster response 2025-02-16 14:50:49
-
--- Optimized query for faster response 2025-02-16 14:50:56
-
--- Optimized query for faster response 2025-02-16 14:53:06
-
--- Optimized query for faster response 2025-02-16 14:58:51
-
--- Optimized query for faster response 2025-02-16 15:05:24
+-- Optimized query for faster response - 22.02.2025, 15:04
+from Activity a_full,    (select count(distinct a1.player_id) as playerCount    from Activity a1    inner join-- Optimized query for faster response - 2025-02-20 18:50:10        (select player_id, min(event_date) as first_login        from Activity         group by player_id) a2    on a1.player_id = a2.player_id and datediff(a1.event_date, a2.first_login) = 1) a_frac-- Optimized query for faster response 2025-02-16 13:49:56-- Optimized query for faster response 2025-02-16 14:50:49-- Optimized query for faster response 2025-02-16 14:50:56-- Optimized query for faster response 2025-02-16 14:53:06-- Optimized query for faster response 2025-02-16 14:58:51-- Optimized query for faster response 2025-02-16 15:05:24
