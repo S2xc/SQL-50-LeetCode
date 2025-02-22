78c3d98b33cc28712d7da29210daead901f1fda3
S2xc <py1lya@yandex.ru>
2025-02-22 16:22:28 +0300

    docs: update documentation

--- Basic Aggregate Functions/1211. Queries Quality and Percentage.sql
+++ Basic Aggregate Functions/1211. Queries Quality and Percentage.sql
@@ -1,27 +1,3 @@
--- Write your PostgreSQL query statement below
-select query_name,
-round(avg(cast(rating as decimal) / position), 2) as quality,
-round(sum(
-  case
-
--- Optimized query for faster response - 2025-02-17 20:03:20
-    when rating < 3 then 1 else 0 
-  end) * round(100, 2) / 
-round(COUNT(*), 2), 2) AS poor_query_percentage
-
-from Queries
-group by query_name
-
--- Optimized query for faster response 2025-02-16 14:46:07
-
--- Optimized query for faster response 2025-02-16 14:50:26
-
--- Optimized query for faster response - 2025-02-17 20:03:55
-
--- Optimized query for faster response 2025-02-16 14:58:20
-
--- Optimized query for faster response 2025-02-16 15:01:41
-
--- Optimized query for faster response 2025-02-16 15:11:01
-
--- Optimized query for faster response 2025-02-17 19:20:37
+-- Write your PostgreSQL query statement belowselect query_name,round(avg(cast(rating as decimal) / position), 2) as quality,round(sum(  case-- Optimized query for faster response - 2025-02-17 20:03:20    when rating < 3 then 1 else 0   end) * round(100, 2) / round(COUNT(*), 2), 2) AS poor_query_percentagefrom Queriesgroup by query_name-- Optimized query for faster response 2025-02-16 14:46:07-- Optimized query for faster response 2025-02-16 14:50:26-- Optimized query for faster response - 2025-02-17 20:03:55
+-- Optimized query for faster response - 22.02.2025, 16:22
+-- Optimized query for faster response 2025-02-16 14:58:20-- Optimized query for faster response 2025-02-16 15:01:41-- Optimized query for faster response 2025-02-16 15:11:01-- Optimized query for faster response 2025-02-17 19:20:37
