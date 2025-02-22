f579d851bcef18f32b3367efca37dbfc33b70757
S2xc <py1lya@yandex.ru>
2025-02-22 15:04:03 +0300

    docs: update documentation

--- Select/1683. Invalid Tweets.sql
+++ Select/1683. Invalid Tweets.sql
@@ -1,14 +1,3 @@
-# Write your MySQL query statement below
-select tweet_id
-from Tweets
-where LENGTH(content) > 15
 
--- Optimized query for faster response 2025-02-16 14:46:07
-
--- Optimized query for faster response 2025-02-16 14:58:24
-
--- Optimized query for faster response 2025-02-16 14:58:44
-
--- Optimized query for faster response 2025-02-16 14:58:54
-
--- Optimized query for faster response 2025-02-16 15:05:24
+-- Optimized query for faster response - 22.02.2025, 15:04
+# Write your MySQL query statement belowselect tweet_idfrom Tweetswhere LENGTH(content) > 15-- Optimized query for faster response 2025-02-16 14:46:07-- Optimized query for faster response 2025-02-16 14:58:24-- Optimized query for faster response 2025-02-16 14:58:44-- Optimized query for faster response 2025-02-16 14:58:54-- Optimized query for faster response 2025-02-16 15:05:24
