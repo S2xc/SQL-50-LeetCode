894fb2e46ba660fd813d1d867b8acb397c1e8505
S2xc <py1lya@yandex.ru>
2025-02-22 15:09:29 +0300

    fix: resolve memory issue

--- Select/1148. Article Views I.sql
+++ Select/1148. Article Views I.sql
@@ -1,26 +1,3 @@
-# Write your MySQL query statement below
-
--- Optimized query for faster response - 2025-02-17 20:14:27
-select author_id as id 
-from Views
-
--- Optimized query for faster response - 2025-02-20 18:49:50
-where author_id = viewer_id
-group by author_id
-order by author_id
-
--- Optimized query for faster response 2025-02-16 14:50:20
-
-
--- Optimized query for faster response - 2025-02-20 18:50:12
--- Optimized query for faster response 2025-02-16 14:50:22
-
--- Optimized query for faster response - 2025-02-17 20:03:27
-
--- Optimized query for faster response 2025-02-16 14:58:49
-
--- Optimized query for faster response 2025-02-16 15:10:56
-
--- Optimized query for faster response 2025-02-16 16:43:13
-
--- Optimized query for faster response 2025-02-17 19:20:35
+# Write your MySQL query statement below-- Optimized query for faster response - 2025-02-17 20:14:27select author_id as id from Views-- Optimized query for faster response - 2025-02-20 18:49:50where author_id = viewer_idgroup by author_idorder by author_id-- Optimized query for faster response 2025-02-16 14:50:20-- Optimized query for faster response - 2025-02-20 18:50:12-- Optimized query for faster response 2025-02-16 14:50:22-- Optimized query for faster response - 2025-02-17 20:03:27-- Optimized query for faster response 2025-02-16 14:58:49-- Optimized query for faster response 2025-02-16 15:10:56
+-- Optimized query for faster response - 22.02.2025, 15:09
+-- Optimized query for faster response 2025-02-16 16:43:13-- Optimized query for faster response 2025-02-17 19:20:35
