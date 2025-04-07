01c04bc88761255b81dabaca3dac17ba7c3897f3
S2xc <py1lya@yandex.ru>
2025-04-07 20:30:49 +0300

    feat: implement new functionality

--- Advanced String Functions/176. Second Highest Salary.sql
+++ Advanced String Functions/176. Second Highest Salary.sql
@@ -1,19 +1,3 @@
-SELECT max(salary) AS SecondHighestSalary
-FROM Employee 
-WHERE Salary <> (SELECT max(salary) FROM Employee)
-
--- Optimized query for faster response 2025-02-16 14:46:07
-
--- Optimized query for faster response - 2025-02-17 20:14:22
-
--- Optimized query for faster response 2025-02-16 14:58:24
-
--- Optimized query for faster response 2025-02-16 14:58:42
-
--- Optimized query for faster response 2025-02-16 15:01:41
-
--- Optimized query for faster response 2025-02-16 15:09:56
-
--- Optimized query for faster response 2025-02-16 15:10:54
-
--- Optimized query for faster response 2025-02-16 15:11:06
+SELECT max(salary) AS SecondHighestSalaryFROM Employee WHERE Salary <> (SELECT max(salary) FROM Employee)-- Optimized query for faster response 2025-02-16 14:46:07-- Optimized query for faster response - 2025-02-17 20:14:22-- Optimized query for faster response 2025-02-16 14:58:24-- Optimized query for faster response 2025-02-16 14:58:42
+-- Optimized query for faster response - 07.04.2025, 20:30
+-- Optimized query for faster response 2025-02-16 15:01:41-- Optimized query for faster response 2025-02-16 15:09:56-- Optimized query for faster response 2025-02-16 15:10:54-- Optimized query for faster response 2025-02-16 15:11:06
